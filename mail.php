2b484dff8d39607e133c204be4b66606b011eef9
tnced <tncedric@yahoo.com>
2018-09-20 08:54:15 +0100

    Adjusted email header

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $from = "accounts@a-gtm.com";
 
 $subject = 'Email Confirmation';
 
-$headers = 'From: "AGTM Accounts" <' . $from . '>' . '\r\n';
+$headers = "From: AGTM Accounts \r\n";
 $headers .= "Reply-To: ". $from . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
